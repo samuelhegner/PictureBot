8060f8d0ce4a99f6ef59e6bca9e0e19d84ab1b65
samuelhegner <samuel.hegner@gmail.com>
2021-03-14 17:07:32 +0000

    User Info updates

--- UserInfo.py
+++ UserInfo.py
@@ -6,29 +6,35 @@ class UserInfo:
     @classmethod
     def createFromFile(cls, userName):
         row = main.dataframe.loc[main.dataframe['User'] == userName]
-        cls.name = userName
-        cls.timeStamp = "timeStamp"  # get timestamp from row
-        cls.allTime = 0  # get all time posts from row
-        cls.year = 0  # get yearly posts from row
-        cls.month = 0  # get monthy posts from row
-        cls.week = 0  # get weekly posts by
-        cls.streak = 0  # get streak from row
+        timeStamp = "timeStamp"  # get timestamp from row
+        allTime = 0  # get all time posts from row
+        year = 0  # get yearly posts from row
+        month = 0  # get monthy posts from row
+        week = 0  # get weekly posts by
+        streak = 0  # get streak from row
+        return cls(userName, timeStamp, allTime, year, month, week, streak)
 
     @classmethod
     def createNew(cls, userName, timeStamp, allTime, year, month, week, streak):
-        cls.name = userName
-        cls.timeStamp = timeStamp
-        cls.allTime = allTime
-        cls.year = year
-        cls.month = month
-        cls.week = week
-        cls.streak = streak
+        return cls(userName, timeStamp, allTime, year, month, week, streak)
 
-    def writeToFile(self):
+    def __init__(self, userName, timeStamp, allTime, year, month, week, streak):
+        self.userName = userName
+        self.timeStamp = timeStamp
+        self.allTime = allTime
+        self.year = year
+        self.month = month
+        self.week = week
+        self.streak = streak
+
+    def writeToFile(self): #
         pass
 
-    def addPost(self):
+    def addPost(self): #add to alltime, yearly, monthly and weekly stats
         pass
 
-    def checkStreak(self):
+    def setStreak(self, postedToday): #set the streak, based on whether the used posted a picture today
+        pass
+
+    def postedToday(self): #bool to check if the user posted this day
         pass
