9be567346a4aa5323943a0ffb661c3fd338e5485
Samuel Hegner <samuel.hegner@gmail.com>
2021-03-17 12:54:55 +0000

    Online bot

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 # bot.py
 import os
 import discord
-
+from keep_alive import keep_alive
 from dotenv import load_dotenv
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,23 +12,22 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print(f'{client.user} is connected to the following guild:\n'
+          f'{guild.name}(id: {guild.id})')
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+
 @client.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to Daily Pics!'
-    )
+    await member.dm_channel.send(f'Hi {member.name}, welcome to Daily Pics!')
+
 
 @client.event
 async def on_message(message):
@@ -37,4 +35,5 @@ async def on_message(message):
         return
 
 
+keep_alive()
 client.run(TOKEN)
